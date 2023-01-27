bca9adac66db607678e6546be07fb156b4a0144e
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-01-27 16:46:16 -0300

    fix(Root): ready to deploy

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -8,6 +8,7 @@ const app = express()
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(express.static('../frontend/build'))
 
 // routers
 app.use(routes)
--- packages/api/src/config.ts
+++ packages/api/src/config.ts
@@ -2,12 +2,13 @@ import 'dotenv/config'
 import NodeCache from 'node-cache'
 
 const port = process.env.PORT ?? 3001
-export const cache = new NodeCache()
+const cache = new NodeCache()
 
 const configs = {
   MONGODB_HOST: process.env.MONGODB_HOST ?? 'mongodb://127.0.0.1:27017/test-db',
   PORT: port,
-  TOKEN_KEY: process.env.TOKEN_KEY!,
+  TOKEN_KEY: process.env.TOKEN_KEY ?? 'test-token'
 }
 
+export { cache }
 export default configs
