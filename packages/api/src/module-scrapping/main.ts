5e74474891c16b2c90c40052bd6e5b789daf3b11
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-01-31 20:29:05 -0300

    added playwright extra

--- packages/api/src/module-scrapping/main.ts
+++ packages/api/src/module-scrapping/main.ts
@@ -1,4 +1,5 @@
-import { chromium } from 'playwright'
+import { chromium } from 'playwright-extra'
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 
 import { scannedAnimeblix } from './scrapping/animeblix'
 import { scannedJkanime } from './scrapping/jkanime'
@@ -6,23 +7,25 @@ import { scannedAnimeFlv } from './scrapping/animeFlv'
 import { scannedMonoschinos } from './scrapping/chinosmonos'
 
 export async function startScrapping() {
-  const browser = await chromium.launch()
+  chromium.use(StealthPlugin())
 
-  let pagesScrapped = await Promise.all([
+  const browser = await chromium.launch({ chromiumSandbox: true })
+
+  let pagesScrapped = await Promise.allSettled([
     scannedAnimeFlv(browser),
     scannedMonoschinos(browser),
     scannedAnimeblix(browser),
     scannedJkanime(browser),
   ])
   const pages = pagesScrapped
-  // .map((page) => {
-  //   if (page.status === 'fulfilled') {
-  //     return page.value
-  //   }
-  //   console.error(`Failed to load ${page.reason} \n`)
-  //   return []
-  // })
-  // .flat()
+    .map((page) => {
+      if (page.status === 'fulfilled') {
+        return page.value
+      }
+      console.error(`Failed to load ${page.reason} \n`)
+      return []
+    })
+    .flat()
   browser.close()
   return pages
 }
