323892139c5f5cdeacbf3f0f048d9bf47606da1e
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-01-31 22:25:07 -0300

    feat(Root): added routes static

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,14 +1,16 @@
+import path from 'path'
 import express from 'express'
 import cors from 'cors'
 import config from './config'
 import routes from './routes'
 const app = express()
-
+const absolutePath = path.join(__dirname, '../../frontend/build')
+console.log(absolutePath)
 // middlewares
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.use(express.static('../frontend/build'))
+app.use('/', express.static(absolutePath))
 
 // routers
 app.use(routes)
