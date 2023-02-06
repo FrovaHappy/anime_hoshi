e18c680b1aa05049bbfaea4de2c023982def8fb0
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-02-06 15:57:08 -0300

    refactor(packages/frontend): code extraction to a function, variable renaming and redirections

--- packages/frontend/src/components/animeInfo.tsx
+++ packages/frontend/src/components/animeInfo.tsx
@@ -1,10 +1,27 @@
-import { useParams } from 'react-router-dom'
-import { AnimeList } from '../../../types'
+import { Navigate, useParams } from 'react-router-dom'
+import { AnimeList, EpisodesContent } from '../../../types'
 import { EpisodesConteiner } from './episodesConteiner'
 import '../styles/animeInfo.scss'
 import { List } from '../../types'
 interface props {
   animeList: AnimeList[]
+}
+const episodesFormat = (listOrdenatedForPage: List, episode: EpisodesContent, namePage: string, keyEpisode: string) => {
+  const url = episode.pagesUrl[namePage]!
+  const update = episode.updateEpisode
+  const element = {
+    url,
+    update,
+    episode: parseInt(keyEpisode),
+  }
+  let listElementModified = listOrdenatedForPage[namePage]
+  if (!listElementModified) {
+    listOrdenatedForPage[namePage] = [element]
+  } else {
+    listElementModified.push(element)
+    listOrdenatedForPage[namePage] = listElementModified
+  }
+  return listOrdenatedForPage
 }
 
 function listPageLinks(anime: AnimeList) {
@@ -16,29 +33,17 @@ function listPageLinks(anime: AnimeList) {
     const keynamePagesArray = Object.keys(episode.pagesUrl)
 
     keynamePagesArray.forEach((namePage) => {
-      const url = episode.pagesUrl[namePage]!
-      const update = episode.updateEpisode
-      const element = {
-        url,
-        update,
-        episode: parseInt(keyepisode),
-      }
-      let listElementModified = list[namePage]
-      if (!listElementModified) {
-        list[namePage] = [element]
-      } else {
-        listElementModified.push(element)
-        list[namePage] = listElementModified
-      }
+      list = episodesFormat(list, episode, namePage, keyepisode)
     })
   })
   return list
 }
+
 export function AnimeInfo({ animeList }: props) {
   const { id } = useParams()
   if (!id) return <div className="anime-info">animeInfo</div>
   const anime = animeList.find((anime) => anime.dataAnilist.id === parseInt(id))
-  if (!anime) return <div className="anime-info"> no found </div>
+  if (!anime) return Navigate({ to: '/', replace: true })
   const list = listPageLinks(anime)
   const color = anime?.dataAnilist.coverImage.color || '#fff'
   return (
