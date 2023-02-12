49a24fbd30cf9d400c32e35c954886e8742fd3bd
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-02-12 20:35:23 -0300

    fix(packages/frontend): new episode item: overflow fix when an element is not rendered

--- packages/frontend/src/utils/renderCondicional.ts
+++ packages/frontend/src/utils/renderCondicional.ts
@@ -4,7 +4,7 @@ function validInsideOf(hours: hours, timestamp: number): boolean {
   return Date.now() - timestamp < hours
 }
 export const isVisibly = (conditional: boolean): React.CSSProperties | undefined => {
-  return conditional ? undefined : { visibility: 'hidden', height: 0, width: 0, overflow: 'hidden' }
+  return conditional ? undefined : { visibility: 'hidden', height: 0, width: 0, margin: 0, overflow: 'hidden' }
 }
 export default {
   validInsideOf,
