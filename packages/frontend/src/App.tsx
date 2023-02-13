f5642d745110127699fa30300fccdd713f6b4159
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-02-13 00:23:32 -0300

    refactor(packages/frontend): restructuring of files and extraction of some components

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,33 +1,14 @@
 import './styles/App.css'
-import { AnimeComponet } from './components/animeList'
-import { useEffect, useState } from 'react'
-import { AnimeList } from '../../types'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
-import { AnimeInfo } from './components/animeInfo'
-import { urlApi } from './config'
+import Home from './pages/home'
 
 function App() {
-  const [animeList, SetAnimelist] = useState<AnimeList[]>([])
-
-  const query = async () => {
-    const data = await fetch(`${urlApi}/animes`)
-      .then((response) => response.json())
-      .catch(() => [])
-    const animelist = await data
-    SetAnimelist(animelist.reverse())
-  }
-
-  useEffect(() => {
-    query()
-  }, [])
-
   return (
     <BrowserRouter>
       <div className="App">
-        <AnimeComponet animes={animeList} />
         <Routes>
-          <Route path={'/'} element={<AnimeInfo animeList={animeList} />} />
-          <Route path={'/:id'} element={<AnimeInfo animeList={animeList} />} />
+          <Route path="/" element={<Home />} />
+          <Route path={'log'} element={<div>log</div>} />
         </Routes>
       </div>
     </BrowserRouter>
--- packages/frontend/src/pages/home/animeInfo.tsx
+++ packages/frontend/src/pages/home/animeInfo.tsx
@@ -1,10 +1,11 @@
-import { Navigate, useParams } from 'react-router-dom'
-import { AnimeList, EpisodesContent } from '../../../types'
+import { Navigate } from 'react-router-dom'
+import { AnimeList, EpisodesContent } from '../../../../types'
 import { EpisodesConteiner } from './episodesConteiner'
-import '../styles/animeInfo.scss'
-import { List } from '../../types'
+import './styles/animeInfo.scss'
+import { List } from '../../../types'
 interface props {
-  animeList: AnimeList[]
+  animes: AnimeList[]
+  id: number | undefined
 }
 const episodesFormat = (listOrdenatedForPage: List, episode: EpisodesContent, namePage: string, keyEpisode: string) => {
   const url = episode.pagesUrl[namePage]!
@@ -39,10 +40,9 @@ function listPageLinks(anime: AnimeList) {
   return list
 }
 
-export function AnimeInfo({ animeList }: props) {
-  const { id } = useParams()
+export function AnimeInfo({ animes, id }: props) {
   if (!id) return <div className="anime-info">animeInfo</div>
-  const anime = animeList.find((anime) => anime.dataAnilist.id === parseInt(id))
+  const anime = animes.find((anime) => anime.dataAnilist.id === id)
   if (!anime) return Navigate({ to: '/', replace: true })
   const list = listPageLinks(anime)
   const color = anime?.dataAnilist.coverImage.color || '#fff'
