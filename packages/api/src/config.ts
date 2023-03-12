882e4aef3d4957b68c6a2aa5e609cd022507e443
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-03-12 20:08:26 -0300

    chore(packages/api): if or if you must declare the environment variables

--- packages/api/src/config.ts
+++ packages/api/src/config.ts
@@ -1,10 +1,9 @@
 import 'dotenv/config'
-const port = process.env.PORT ?? 3001
 
 const configs = {
-  MONGODB_HOST: process.env.MONGODB_HOST ?? 'mongodb://127.0.0.1:27017/test-db',
-  PORT: port,
-  TOKEN_KEY: process.env.TOKEN_KEY ?? 'test-token',
+  MONGODB_HOST: process.env.MONGODB_HOST!,
+  PORT: process.env.PORT!,
+  TOKEN_KEY: process.env.TOKEN_KEY!,
   CRYPTO_KEY: process.env.CRYPTO_KEY!,
 }
 
