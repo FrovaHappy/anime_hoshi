7864040bc26c611c94c85ff7bca6684b75986126
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-03-16 14:33:58 -0300

    fix(packages/api): fixed episode value that was saved as null

--- packages/api/src/module-scrapping/modules/fomartItemScraper.ts
+++ packages/api/src/module-scrapping/modules/fomartItemScraper.ts
@@ -3,6 +3,6 @@ import { EpisodeNumber } from '../../Enum'
 
 export function formartItemScraper(itemScrap: InfoEpisodeRecovered) {
   itemScrap.title = itemScrap.title.replace(/["]/g, '')
-  itemScrap.episode = isNaN(itemScrap.episode) ? EpisodeNumber.lastEpisodeNotFound : itemScrap.episode
+  if (isNaN(itemScrap.episode) || itemScrap.episode === null) itemScrap.episode = EpisodeNumber.lastEpisodeNotFound
   return itemScrap
 }
