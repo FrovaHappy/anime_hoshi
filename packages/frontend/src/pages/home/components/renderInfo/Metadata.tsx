a6f3afa9579cb41125ab428a583e824c9d7397c1
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-04-27 18:46:13 -0300

    feat(packages/frontend): duracion de los epidodios agregado

--- packages/frontend/src/pages/home/components/renderInfo/Metadata.tsx
+++ packages/frontend/src/pages/home/components/renderInfo/Metadata.tsx
@@ -3,8 +3,15 @@ import { AnimeList } from '../../../../../../types'
 type Props = {
   anime: AnimeList
 }
+function formatTime(duration?: number): string {
+  if (!duration) return ''
+  const inHours = duration / 60
+  const hasHour = Math.floor(inHours) > 1
+  if (hasHour) return `${inHours.toFixed(1)} Hora/s`
+  return `${duration} Minuto/s`
+}
 function Metadata({ anime }: Props) {
-  const { episodes, id, format, status, averageScore } = anime.dataAnilist
+  const { episodes, id, format, status, averageScore, duration } = anime.dataAnilist
   const renderMetadata = (hasContent: boolean, content: string) => {
     return hasContent ? <div className="renderInfo__metadata--item">{content}</div> : null
   }
@@ -16,6 +23,7 @@ function Metadata({ anime }: Props) {
       {renderMetadata(Boolean(episodes), `${episodes} Episodios`)}
       {renderMetadata(Boolean(format), `${format}`)}
       {renderMetadata(Boolean(status), `${status}`)}
+      {renderMetadata(Boolean(duration), formatTime(duration))}
     </div>
   )
 }
