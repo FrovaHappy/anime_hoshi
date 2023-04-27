4dae79a23909813631f14445f553d86a825da8fe
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-04-26 23:04:45 -0300

    refactor(packages/api): uso de Boolean para validar los falsy

--- packages/api/src/module-scrapping/modules/fomartItemScraper.ts
+++ packages/api/src/module-scrapping/modules/fomartItemScraper.ts
@@ -3,6 +3,6 @@ import { EpisodeNumber } from '../../Enum'
 
 export function formartItemScraper(itemScrap: InfoEpisodeRecovered) {
   itemScrap.title = itemScrap.title.replace(/["]/g, '')
-  if (isNaN(itemScrap.episode) || itemScrap.episode === null) itemScrap.episode = EpisodeNumber.lastEpisodeNotFound
+  if (!Boolean(itemScrap.episode)) itemScrap.episode = EpisodeNumber.lastEpisodeNotFound
   return itemScrap
 }
