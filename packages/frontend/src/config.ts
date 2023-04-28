61800ff658f27bc776a03810db4f496d040c9daf
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-04-28 18:13:18 -0300

    chore(packages/frontend): env configurados
    dos perfiles de las env agregadas (LOCAL y CLOUN)
    
    eliminacion de dependencias no utilizadas

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -1,3 +1,3 @@
-const urlApi = 'https://scraping-api-production.up.railway.app'
-
+const urlApi = process.env.REACT_APP_URLAPI //'https://scraping-api-production.up.railway.app'
+console.log(urlApi)
 export { urlApi }
