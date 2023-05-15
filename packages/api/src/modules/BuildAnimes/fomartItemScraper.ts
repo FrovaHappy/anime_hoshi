442c43ec007ed935c6a84bb891829c7a30c6bf14
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-05-15 10:32:28 -0300

    🚧 titulo de scrap formateado correctamente

--- packages/api/src/modules/BuildAnimes/fomartItemScraper.ts
+++ packages/api/src/modules/BuildAnimes/fomartItemScraper.ts
@@ -1,8 +1,15 @@
 import { InfoEpisodeRecovered } from '../../../../types'
 import { EpisodeNumber } from '../../Enum'
-
+function remplaseTitle(title: string) {
+  title = title.replace(/["]/g, '')
+  title = title.replace('2da Temporada', '2nd Season')
+  title = title.replace('Temporada 2', '2nd Season')
+  title = title.replace('Temporada', 'Season')
+  title = title.replace('Parte', 'Part')
+  return title
+}
 export function formartItemScraper(itemScrap: InfoEpisodeRecovered) {
-  itemScrap.title = itemScrap.title.replace(/["]/g, '')
+  itemScrap.title = remplaseTitle(itemScrap.title)
   if (!Boolean(itemScrap.episode)) itemScrap.episode = EpisodeNumber.lastEpisodeNotFound
   return itemScrap
 }
