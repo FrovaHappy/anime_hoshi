36b46467ff3f2b3121223678d21e5f79a2b13c95
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-07-16 17:00:50 -0300

    🔥(api) eliminado modulo anterior de webPush 🔥(api) eliminado modulo anterior de [Routes]/episodes

--- packages/api/src/routes/episodes.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import auth from '../middleware/auth'
-import { validateEpisodes } from '../validators/episodes'
-import { putEpisodes } from '../controllers/espisodes.controler'
-const router = Router()
-
-router.put('/', auth.checkRole.admin, validateEpisodes, putEpisodes)
-
-export default router
