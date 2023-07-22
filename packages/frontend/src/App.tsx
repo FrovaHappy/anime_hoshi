acc72d2ac9585a017ddd9e36d39ac2b399b3fd68
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-07-22 12:02:20 -0300

    🔥(front) removido componente Nav ♻️(front) cambio en el export de los Icons ✏️(front) cambio en los tipos de Icons

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,13 +2,11 @@ import './styles/App.css'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Home from './pages/home'
 import User from './pages/user'
-import Nav from './Nav'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path={'log'} element={<div>log</div>} />
--- packages/frontend/src/Icons/index.tsx
+++ packages/frontend/src/Icons/index.tsx
@@ -4,28 +4,10 @@ import IconGear from './Gear'
 import IconClose from './Close'
 import IconBug from './Bug'
 import IconNew from './New'
-import { IconPointNav, IconPointNavActive } from './PointNav'
 import IconRotate from './Rotate'
 import IconExternalLink from './ExternalLink'
 import IconBars from './Bars'
 
-interface IconProps {
-  className?: string
-  style?: React.CSSProperties
-  iconName: Icon
-}
-type Icon =
-  | 'IconHome'
-  | 'IconCircleInfo'
-  | 'IconGear'
-  | 'IconNew'
-  | 'IconBug'
-  | 'IconClose'
-  | 'IconPointNav'
-  | 'IconPointNavActive'
-  | 'IconRotate'
-  | 'IconExternalLink'
-  | 'IconBars'
 const icons = {
   IconHome,
   IconCircleInfo,
@@ -33,19 +15,23 @@ const icons = {
   IconClose,
   IconBug,
   IconNew,
-  IconPointNav,
-  IconPointNavActive,
   IconRotate,
   IconExternalLink,
   IconBars,
 }
 
+interface IconProps {
+  className?: string
+  style?: React.CSSProperties
+  iconName: keyof typeof icons
+}
+
 const FilterIcon = (iconProps: IconProps) => {
   const { className, iconName, style } = iconProps
-  const { path, viewBox } = icons[iconName]
+  const { jsx, viewBox } = icons[iconName]
   return (
     <svg className={className} style={style} viewBox={viewBox} xmlns="http://www.w3.org/2000/svg" fill="currentColor">
-      {path}
+      {jsx}
     </svg>
   )
 }
