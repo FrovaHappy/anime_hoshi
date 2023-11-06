f8fc3faa6875abeb1c10e52c652441dde714680e
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-11-06 17:32:15 -0300

    🚚[front] movido dashboard a dashboardPages

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,7 +2,7 @@ import './styles/App.css'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Home from './pages/home'
 import User from './pages/user'
-import Dashboard from './pages/dashboard'
+import DashboardPages from './pages/dashboardPages'
 
 function App() {
   return (
@@ -11,7 +11,17 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path={'setting'} element={<User />} />
-          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='dashboard'>
+            <Route
+              path=''
+              element={
+                <>
+                  <div>dashboard</div>
+                </>
+              }
+            />
+            <Route path='pages' element={<DashboardPages />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
