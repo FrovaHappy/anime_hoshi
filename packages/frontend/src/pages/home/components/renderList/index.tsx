afe166bba1f0b7c6ad63f717722a6d5c33f95fef
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-11-08 16:43:26 -0300

    ✨[front] barra de búsqueda agregado

--- packages/frontend/src/pages/home/components/renderList/index.tsx
+++ packages/frontend/src/pages/home/components/renderList/index.tsx
@@ -4,26 +4,55 @@ import { FetchLoading } from './fetchLoading'
 import TargetAnime from './targetAnime'
 import useFetch from '../../../../hooks/useFetchNew'
 import { urlApi } from '../../../../config'
+import ErrorComponent from '../../../../components/Error'
+import { useState } from 'react'
+import type { AnimeMinified } from '../../../../../../types/Anime'
+import type React from 'react'
 
 function renderList() {
   const { animesMinfied, setAnimesMinfied } = useContextAnimes()
-  const { load, error, contents } = useFetch({
+  const [filter, setFilter] = useState<AnimeMinified[] | null>(null)
+  const { load, error, contents, errorCode } = useFetch({
     query: { url: `${urlApi ?? ''}/animes`, method: 'GET' },
     deps: [],
     conditional: animesMinfied.length === 0
   })
   if (load) return <FetchLoading />
-  if (error) return <div>Error Con la Api 😐{error}</div>
+  if (error) return <ErrorComponent code={errorCode} message={error} />
   if (contents) setAnimesMinfied(contents.animes)
-  if (!animesMinfied) return <div>Error Inesperado </div>
+  if (!animesMinfied) return <ErrorComponent code={500} message='error no controlado' />
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    const search = e.target.value
+    if (search.length === 0) {
+      setFilter(null)
+      return
+    }
+    const data = animesMinfied.filter(ani => ani.title.toLowerCase().includes(search.toLowerCase()))
+    setFilter(data)
+  }
+  const render = () => {
+    if (filter) {
+      return filter.map(animeMinified => {
+        return <TargetAnime key={animeMinified.id} thisAnime={animeMinified} />
+      })
+    }
+    return animesMinfied.map(animeMinified => {
+      return <TargetAnime key={animeMinified.id} thisAnime={animeMinified} />
+    })
+  }
   return (
-    <div className='renderList'>
-      <div className='renderList__targets'>
-        {animesMinfied.map(animeMinified => {
-          return <TargetAnime key={animeMinified.id} thisAnime={animeMinified} />
-        })}
+    <>
+      <div className='renderList'>
+        <input
+          type='text'
+          placeholder='Filtra por nombre...'
+          className='input renderList__search'
+          onChange={handleChange}
+        />
+        <div className='renderList__targets'>{render()}</div>
       </div>
-    </div>
+    </>
   )
 }
 
