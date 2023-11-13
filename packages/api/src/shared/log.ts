86a66d2a8ced5fb34fd6b5e9b152cd806d610bda
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-11-13 18:33:27 -0300

    ♻️[api] cambiado el tipo de archivo del logs 🧑‍💻[api] ignorado .logs/ en nodemon ✨[api] colocado un limite de archivo (15 días) 🚧[api] construyendo la nueva implementación del logger

--- packages/api/src/shared/log.ts
+++ packages/api/src/shared/log.ts
@@ -1,23 +1,40 @@
-import { writeFile, readFile } from 'fs/promises'
-import path from 'path'
-import type { Log } from '../../../types'
+import { writeFile, readFile, readdir, rm } from 'fs/promises'
+import type { Log, LogType } from '../../../types'
 
-export default async function log ({ type, message, content }: Log) {
+async function read(pathFile: string) {
+  try {
+    return JSON.parse(await readFile(pathFile, { encoding: 'utf-8' }))
+  } catch {
+    return []
+  }
+}
+async function cutCollection(limit: number) {
+  const files = (await readdir('logs/')).filter(file => file.endsWith('.json'))
+  const deletesFiles = files.slice(limit)
+  for (const file of deletesFiles) {
+    await rm('logs/' + file)
+  }
+}
+export default async function log({ type, message, content }: Log) {
   const date = new Date()
   const day = date.getUTCDate()
   const month = date.getUTCMonth()
   const year = date.getUTCFullYear()
-  const pathFile = path.join(__dirname, `../../log/${year}-${month}-${day}.txt`)
+  const pathFile = `logs/${year}-${month}-${day}.json`
+  await cutCollection(15)
 
-  let logFile: any[]
-  let oldLogFile: [string, any[]] = [pathFile, []]
-  try {
-    logFile = JSON.parse(await readFile(pathFile, { encoding: 'utf8' }))
-    oldLogFile = [pathFile, logFile]
-  } catch {
-    oldLogFile[0] === pathFile ? (logFile = oldLogFile[1]) : (logFile = [])
-  }
+  const logFile = await read(pathFile)
 
   const newLog = { type, message, content, timestamp: Date.now() }
-  await writeFile(pathFile, JSON.stringify([newLog, ...logFile]).replace(';', ''))
+  await writeFile(pathFile, JSON.stringify([newLog, ...logFile]).replace(';', ''), { encoding: 'utf-8' })
+}
+async function buildLogger(type: LogType) {
+  return async ({ content, message }: Omit<Log, 'type'>) => {
+    await log({ type, content, message })
+  }
+}
+export const logger = {
+  warn: buildLogger('warning'),
+  error: buildLogger('error'),
+  info: buildLogger('info')
 }
