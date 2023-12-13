fc853498310b0c13bfce84ac0695038c7224b200
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-12-13 14:22:37 -0300

    [front] agregando analíticas de vercel

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -2,10 +2,12 @@ import ReactDOM from 'react-dom/client'
 import './styles/index.css'
 import App from './App'
 import { StrictMode } from 'react'
+import { Analytics } from '@vercel/analytics/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 )
