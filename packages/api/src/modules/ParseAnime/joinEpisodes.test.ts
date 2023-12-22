0076909fc058f432b5523534b96725bef17b8b03
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-12-21 21:06:50 -0300

    🧪[api] test cuando el link es diferente
    también se cambio el comportamiento en caso de no detectar cambios, ahora se retorna null

--- packages/api/src/modules/ParseAnime/joinEpisodes.test.ts
+++ packages/api/src/modules/ParseAnime/joinEpisodes.test.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import fs from 'fs'
 import path from 'path'
-import { type Episode, type Anime } from '../../../../types/Anime'
+import { type Anime } from '../../../../types/Anime'
 import joinEpisodes from './joinEpisodes'
 import { type LangSupport, type ScrapEpisode } from '../../../../types/ScrapEpisode'
 const anime: Anime = JSON.parse(fs.readFileSync(path.join(__dirname, '/anime.mock.json'), { encoding: 'utf8' }))
@@ -13,8 +14,7 @@ describe('Join Episodes', () => {
 
   test('when the scrap is equal', () => {
     const result = joinEpisodes({ anime, episodeScraper: baseEpisode, namePage })
-    expect(Object.keys(result.pages)).length(2)
-    expect(result.pages[namePage].episodes[0]).toEqual({ episode, link, lastUpdate: 0 } satisfies Episode)
+    expect(result).toBeNull()
   })
 
   test('when the episode is different', () => {
@@ -24,11 +24,25 @@ describe('Join Episodes', () => {
       anime,
       namePage
     })
-    const episodes = result.pages[namePage].episodes
+    const episodes = result!.pages[namePage].episodes
     expect(episodes).length(2)
     expect(episodes[0].episode).toBe(2)
     expect(episodes[0].link).toBe(link)
     expect(episodes[0].lastUpdate).toBeTypeOf('number')
     expect(episodes[1]).toEqual({ episode, link, lastUpdate: 0 })
+  })
+  test('when the link is different', () => {
+    const linkModifier = '/linkModifier'
+    const episodeScraper = { title, episode, lang, link: linkModifier }
+    const result = joinEpisodes({
+      episodeScraper,
+      anime,
+      namePage
+    })
+    const episodes = result!.pages[namePage].episodes
+    expect(episodes).length(1)
+    expect(episodes[0].episode).toBe(episode)
+    expect(episodes[0].lastUpdate).toBeTypeOf('number')
+    expect(episodes[0].link).toBe(linkModifier)
   })
 })
--- packages/api/src/modules/ParseAnime/joinEpisodes.ts
+++ packages/api/src/modules/ParseAnime/joinEpisodes.ts
@@ -11,8 +11,11 @@ export default function joinEpisodes({ anime, episodeScraper, namePage }: Params
   const pages = animeCopy.pages
   const page = pages[namePage]
   const episodes = page.episodes
+  let hasUpdated = false
+
   const notFound = -1
   const episodeIndex = page.episodes.findIndex(e => e.episode === episodeScraper.episode)
+
   if (episodeIndex === notFound) {
     const episode: Episode = {
       episode: episodeScraper.episode,
@@ -20,7 +23,13 @@ export default function joinEpisodes({ anime, episodeScraper, namePage }: Params
       link: episodeScraper.link
     }
     page.episodes = [episode, ...episodes]
+    hasUpdated = true
+  } else {
+    const episode = page.episodes[episodeIndex]
+    hasUpdated = episode.link !== episodeScraper.link
+    episode.link = episodeScraper.link
   }
+  if (!hasUpdated) return null
   pages[namePage] = page
   animeCopy.pages = pages
   return animeCopy
