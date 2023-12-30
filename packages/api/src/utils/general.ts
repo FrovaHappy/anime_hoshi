2e99ecf81028e4c327be6f40b3f7c3d47b41b989
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2023-12-30 18:25:48 -0300

    [api] mayorLastUpdate agregado

--- packages/api/src/utils/general.ts
+++ packages/api/src/utils/general.ts
@@ -1,3 +1,14 @@
+import { type Anime } from '../../../types/Anime'
+
 export function copyDeepObject<T>(obj: T) {
   return JSON.parse(JSON.stringify(obj)) as T
+}
+export function mayorLastUpdate(a: Anime) {
+  let lastUpdate = 0
+  const namePages = Object.keys(a.pages)
+  namePages.forEach(namePage => {
+    const page = a.pages[namePage]
+    if (lastUpdate < page.lastUpdate) lastUpdate = page.lastUpdate
+  })
+  return lastUpdate
 }
