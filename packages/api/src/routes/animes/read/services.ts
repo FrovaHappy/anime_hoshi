70a7b0b55cdf6266b4c89a4425540d3e24bc1e26
FrovaHappy <61207925+FrovaHappy@users.noreply.github.com>
2024-01-03 16:17:15 -0300

    [api] fix orden de los animes recientes
    ahora se ordena correctamente

--- packages/api/src/routes/animes/read/services.ts
+++ packages/api/src/routes/animes/read/services.ts
@@ -7,7 +7,7 @@ let animesMinifiedCache: AnimeMinified[] = []
 export function getAnimesMinified() {
   const animes: Anime[] = animesCache.getCache()
   lastUpdate = animesCache.get().updated ? Date.now() : lastUpdate
-  animesMinifiedCache = animes.map(anime => animeMinified(anime))
+  animesMinifiedCache = animes.map(anime => animeMinified(anime)).sort((a, b) => b.lastUpdate - a.lastUpdate)
   return {
     lastUpdate,
     animesMinified: animesMinifiedCache
