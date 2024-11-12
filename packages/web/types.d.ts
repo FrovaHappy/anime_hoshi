f63d0be7cd3a85201c27b49624c24d198927154e
frova <happypro5.0@gmail.com>
2024-11-12 17:09:33 -0300

    fix(web): :ambulance: restored types.d file delete

---
+++ packages/web/types.d.ts
@@ -0,0 +1,27 @@
+import type { PayloadAnimeNof } from "../types/Payloads";
+export type ObjectDynamic<T> = Record<string, T>;
+
+export interface notificationProperty {
+	hasAccept: boolean;
+	updated?: number;
+}
+export type UseState<T> = [T, (key: T) => void];
+export interface InAwait {
+	cantRemitted: number; // cant of emitted the notification
+	expireIn: number;
+	created: number;
+	anime: PayloadAnimeNof;
+	pastLengthPages: number;
+}
+export type NotificationInAwait = InAwait[];
+export interface NotificationsInAired {
+	maxRemitted: number;
+	delay: number;
+	expireIn: number;
+	minPages: number;
+}
+export interface User {
+	newToken: string;
+	username: string;
+	roles: string[];
+}
